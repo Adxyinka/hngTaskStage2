695f70dc71e00724c442f09af799ff26b638db0d
Adeyinka Yusuf <Adeyinkaryusuf@gmail.com>
2021-08-19 17:40:18 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+//importing dependencies
+
+const express = require("express")
+
+const app = express();
+
+const path = require('path');
+
+var mongoose = require("mongoose");
+
+var bodyParser = require("body-parser");
+
+
+
+// Calling form.js from models
+
+var Form = require("./models/form");
+
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Connecting to database
+
+mongoose.connect("mongodb://localhost/resume", {
+
+  useNewUrlParser: true,
+
+  useUnifiedTopology: true
+
+});
+
+
+
+//middlewares
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+//rendering form.ejs
+
+app.get("/", function(req, res) {
+
+  res.render("resume");
+
+});
+
+
+
+// form submission
+
+app.get('/result', (req, res) => {
+
+  res.render('result');
+
+});
+
+
+
+//creating form
+
+app.post("/", function(req, res) {
+
+  var username = req.body.username;
+
+  var email = req.body.email;
+
+  var message = req.body.message;
+
+  var f = {
+    username: username,
+    email: email,
+    message: message,
+  };
+
+  Form.create(f, function(err, newlyCreatedForm) {
+
+    if (err)
+
+    {
+
+      console.log(err);
+
+    } else {
+
+      res.redirect("/result");
+
+    }
+
+  });
+
+});
+
+
+
+// Starting the server at port 3000
+
+app.listen(3000)
+
+});
---
+++ models/form.js
@@ -0,0 +1,21 @@
+//Requiring mongoose package
+
+var mongoose=require("mongoose");
+
+  
+
+// Schema
+
+var formSchema=new mongoose.Schema({
+
+    username : String,
+
+    email    : String,
+
+    Message   : String
+
+});
+
+  
+
+module.exports=mongoose.model("Form",formSchema);
